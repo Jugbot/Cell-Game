d576853063b2ca6552abe404122d9e7624cda07f
Lucas <lp1935@nyu.edu>
2019-06-22 08:38:32 -0400

    :cyclone:

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 Camera = require "hump.camera"
+Squish = require "squish"
+
 
 function love.load()
   love.physics.setMeter(64) --the height of a meter our worlds will be 64px
@@ -19,27 +21,10 @@ function love.load()
   objects.ground.shape4 = love.physics.newRectangleShape(-w/2, 0, 50, h, 0) --make a rectangle with a width of 650 and a height of 50
   objects.ground.fixture4 = love.physics.newFixture(objects.ground.body, objects.ground.shape4); --attach shape to body
 
-  --let's create a ball
-  objects.ball = {}
-  objects.ball.body = love.physics.newBody(world, 0, 0, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-  objects.ball.shape = love.physics.newCircleShape(20) --the ball's shape has a radius of 20
-  objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1) -- Attach fixture to body and give it a density of 1.
-  -- objects.ball.fixture:setRestitution(0.9) --let the ball bounce
-  --another one
-  objects.ball2 = {}
-  objects.ball2.body = love.physics.newBody(world, 50, 50, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-  objects.ball2.shape = love.physics.newCircleShape(20) --the ball's shape has a radius of 20
-  objects.ball2.fixture = love.physics.newFixture(objects.ball2.body, objects.ball2.shape, 1) -- Attach fixture to body and give it a density of 1.
-  -- objects.ball2.fixture:setRestitution(0.9) --let the ball bounce
+  squish = Squish
+  squish:init(50,50,100,50)
 
-	-- love.physics.newPrismaticJoint( objects.ball.body, objects.ball2.body, 0, 0, 1, 1, true )
-	-- love.physics.newRevoluteJoint(objects.ball.body, objects.ball2.body, 0, 0, true )
-	joint = love.physics.newWeldJoint(objects.ball.body, objects.ball2.body, 0, 0, true )
-	joint:setFrequency(0.5)
-	 -- <1 springlike
-	joint:setDampingRatio(10)
-
-  camera = Camera(objects.ball.body:getX(), objects.ball.body:getY())
+  camera = Camera(squish:getX(), squish:getY())
 
 end
 
@@ -47,19 +32,11 @@ end
 function love.update(dt)
   world:update(dt) --this puts the world into motion
 
-  local dx,dy = objects.ball.body:getX() - camera.x, objects.ball.body:getY() - camera.y
+  local dx,dy = squish:getX() - camera.x, squish:getY() - camera.y
   camera:move(dx * dt, dy * dt)
 
-  --here we are going to create some keyboard events
-  if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
-    objects.ball.body:applyForce(400, 0)
-  elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
-    objects.ball.body:applyForce(-400, 0)
-  elseif love.keyboard.isDown("up") then --press the up arrow key to set the ball in the air
-    objects.ball.body:applyForce(0, -400)
-  elseif love.keyboard.isDown("down") then --press the up arrow key to set the ball in the air
-    objects.ball.body:applyForce(0, 400)
-  end
+  squish:update()
+
 end
 
 function love.draw()
@@ -70,10 +47,7 @@ function love.draw()
   love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape3:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
   love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape4:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
 
-  love.graphics.setColor(0.76, 0.18, 0.05) --set the drawing color to red for the ball
-  love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+  squish:draw()
 
-  love.graphics.setColor(0.76, 0.18, 0.55) --set the drawing color to red for the ball
-  love.graphics.circle("fill", objects.ball2.body:getX(), objects.ball2.body:getY(), objects.ball2.shape:getRadius())
 	camera:detach()
 end
---
+++ squish.lua
@@ -0,0 +1,74 @@
+
+local Squish = {}
+
+function Squish:init(x,y,size,num) 
+  self.fidelity = num
+  self.center = {}
+  self.nodes = {}
+  local body = love.physics.newBody(world, x, y, "dynamic")
+  self.center.body = body
+  self.center.shape = love.physics.newCircleShape(20)
+  self.center.fixture = love.physics.newFixture(self.center.body, self.center.shape, 1)
+
+  last = false
+  for i=1,self.fidelity do 
+    local angle = (2*math.pi)/self.fidelity*i;
+    
+    local posx = x+size*math.cos(angle);
+    local posy = y+size*math.sin(angle);
+    self.nodes[i] = {}
+    local body = love.physics.newBody(world, posx, posy, "dynamic")
+    body:setBullet(true)
+    body:setAngularDamping(5000)
+    body:setMass(1)
+    self.nodes[i].body = body
+    self.nodes[i].shape = love.physics.newCircleShape(10)
+    local fixture = love.physics.newFixture(self.nodes[i].body, self.nodes[i].shape, 1)
+    fixture:setCategory(2)
+    fixture:setMask(2)
+    self.nodes[i].fixture = fixture
+    joint = love.physics.newDistanceJoint(self.nodes[i].body, self.center.body, posx, posy, posx, posy, true )
+    joint:setDampingRatio(0.2)
+    joint:setFrequency(0.8)
+  end
+
+  for i = 1, #self.nodes do
+    if i < #self.nodes then
+      local j = love.physics.newDistanceJoint(self.nodes[i].body, self.nodes[i+1].body, self.nodes[i].body:getX(), self.nodes[i].body:getY(),
+      self.nodes[i+1].body:getX(), self.nodes[i+1].body:getY(), false);
+      self.nodes[i].joint2 = j;
+    else
+      local j = love.physics.newDistanceJoint(self.nodes[i].body, self.nodes[1].body, self.nodes[i].body:getX(), self.nodes[i].body:getY(),
+      self.nodes[1].body:getX(), self.nodes[1].body:getY(), false);
+      self.nodes[i].joint3 = j;
+    end
+  end
+end
+
+function Squish:draw()
+  love.graphics.setColor(0.76, 0.18, 0.55) --set the drawing color to red for the ball
+  love.graphics.circle("fill", self.center.body:getX(), self.center.body:getY(), self.center.shape:getRadius())
+  love.graphics.setColor(0.76, 0.18, 0.05) --set the drawing color to red for the ball
+
+  for i=1, #self.nodes do 
+    love.graphics.circle("fill", self.nodes[i].body:getX(), self.nodes[i].body:getY(), self.nodes[i].shape:getRadius())
+  end
+end
+
+function Squish:update() 
+  --here we are going to create some keyboard events
+  if love.keyboard.isDown("d") then --press the right arrow key to push the ball to the right
+    self.center.body:applyForce(400, 0)
+  elseif love.keyboard.isDown("a") then --press the left arrow key to push the ball to the left
+    self.center.body:applyForce(-400, 0)
+  elseif love.keyboard.isDown("w") then --press the up arrow key to set the ball in the air
+    self.center.body:applyForce(0, -400)
+  elseif love.keyboard.isDown("s") then --press the up arrow key to set the ball in the air
+    self.center.body:applyForce(0, 400)
+  end
+end
+
+function Squish:getX() return self.center.body:getX() end
+function Squish:getY() return self.center.body:getY() end
+
+return Squish
